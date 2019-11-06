b13b3f78926cc9210c2f4a11ed72796bab1b5ad9
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-06 15:15:30 +0100

    redering api JSON data to DOM

--- src/components/SteelApp.jsx
+++ src/components/SteelApp.jsx
@@ -1,19 +1,23 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 function SteelApp() {
+  let sensrData = ''
+  const  [apiData,setApiData ]= useState({})
   useEffect(() => {
 
   const url = "http://127.0.0.1:7410/data"; // site that doesn’t send Access-Control-*
 
     fetch(url).then( response => response.json().then( data => {
       console.log('fetch',data)
+      sensrData = data.sensor_data
+      setApiData(data)
     })
     ).catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
   },[])
 
   return (
-    <div className="SteelApp">
-      hi
+    <div className="steel-app">
+    <div className="sensor-api">{JSON.stringify(apiData)}</div>
     </div>
   );
 }
