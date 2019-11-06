622d70a6ae01cd09bced9f970e4d04b9da369308
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-06 01:01:44 +0100

    add toolbar nav UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import SteelApp from './components/SteelApp'
+import BodyApp from './components/Body'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SteelApp/>
+      <BodyApp/>
+      {/* <SteelApp/> */}
     </div>
   );
 }
--- src/components/SteelApp.jsx
+++ src/components/SteelApp.jsx
@@ -2,14 +2,17 @@ import React, {useEffect} from 'react';
 
 function SteelApp() {
   useEffect(() => {
-    fetch('http://127.0.0.1:7410/data', {
+
+  const url = "http://127.0.0.1:7410/data"; // site that doesn’t send Access-Control-*
+
+    fetch(url, {
+      method: 'PATCH',
       headers: {
-        'Content-Type': 'text/html',
         'Access-Control-Allow-Origin':'*'
       }}).then( response => response.json().then( data => {
       console.log('fetch',data)
     })
-    );
+    ).catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
   },[])
 
   return (
