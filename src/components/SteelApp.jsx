25a75fedd630736dbf27e3a7e64c7ba7d879b390
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-06 15:22:14 +0100

    making API fetch async

--- src/components/SteelApp.jsx
+++ src/components/SteelApp.jsx
@@ -2,17 +2,22 @@ import React, {useEffect, useState} from 'react';
 
 function SteelApp() {
   let sensrData = ''
-  const  [apiData,setApiData ]= useState({})
+  const [apiData,setApiData] = useState({})
+  const [errApi, setErrApi] = useState(false)
+
+  async function fetcherData() {
+    const url = "http://127.0.0.1:7410/data"; // site that doesn’t send Access-Control-*
+
+    fetch(url)
+    .then( response => response.json())
+    .then( data => setApiData(data)) // set data into state
+    .catch(err => setErrApi(err));
+  }
+
+  // on mount call fetch API 
+  // set data into state
   useEffect(() => {
-
-  const url = "http://127.0.0.1:7410/data"; // site that doesn’t send Access-Control-*
-
-    fetch(url).then( response => response.json().then( data => {
-      console.log('fetch',data)
-      sensrData = data.sensor_data
-      setApiData(data)
-    })
-    ).catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
+    fetcherData();
   },[])
 
   return (
