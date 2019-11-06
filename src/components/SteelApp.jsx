efbcf5f1f746851133c2687d21c0fa6427c647c2
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-06 15:05:40 +0100

    setup SASS precompiller. Fetch flash API data working!

--- src/components/SteelApp.jsx
+++ src/components/SteelApp.jsx
@@ -5,11 +5,7 @@ function SteelApp() {
 
   const url = "http://127.0.0.1:7410/data"; // site that doesn’t send Access-Control-*
 
-    fetch(url, {
-      method: 'PATCH',
-      headers: {
-        'Access-Control-Allow-Origin':'*'
-      }}).then( response => response.json().then( data => {
+    fetch(url).then( response => response.json().then( data => {
       console.log('fetch',data)
     })
     ).catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
